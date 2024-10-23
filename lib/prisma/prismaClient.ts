109eb2e8a0e5ad10683aef14bdff182985ee3ddf
piyushgambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-10-23 21:21:16 +0530

    refactor: Update prismaClient.ts to improve code structure and readability

--- lib/prisma/prismaClient.ts
+++ lib/prisma/prismaClient.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-let prisma: PrismaClient;
-
-const createPrismaClient = () => {
-  return new PrismaClient().$extends(withAccelerate());
+// Helper function to create a PrismaClient
+const createPrismaClient = (): PrismaClient => {
+  // Cast the extended client back to PrismaClient type
+  return new PrismaClient().$extends(withAccelerate()) as PrismaClient;
 };
+
+let prisma: PrismaClient;
 
 if (process.env.ENVIRONMENT === 'production') {
   prisma = createPrismaClient();
